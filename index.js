985b8cc578ca72f85ecec911d5a1f93f5a56ac78
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-05-25 03:48:28 +0600

    cart post api

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
 
     const menuCollection = client.db("bistroRestaurantDB").collection("menu");
     const reviewCollection = client.db("bistroRestaurantDB").collection("reviews");
+    const cartCollection = client.db("bistroRestaurantDB").collection("carts");
 
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray();
@@ -37,6 +38,11 @@ async function run() {
         res.send(result);
     })
 
+    app.post("/carts", async(req, res)=>{
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
