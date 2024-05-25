ddc54c4383c91d05fcdaa6d08dc9201395db50e6
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-05-26 02:22:52 +0600

    delete api for deleting cart item

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 // middleware
 app.use(cors())
@@ -48,6 +48,13 @@ async function run() {
       const email = req.query.email;
       const query = {email: email}
       const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.delete("/carts/:id", async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
       res.send(result);
     })
     // Send a ping to confirm a successful connection
