32f27acf7e809cf0298d1b492fe0f2a5d004e9f3
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-05-27 03:39:22 +0600

    delete api for delete user from all users

--- index.js
+++ index.js
@@ -48,7 +48,15 @@ async function run() {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    })
     
+    //menu apis
     app.get('/menu', async(req, res)=>{
         const result = await menuCollection.find().toArray();
         res.send(result);
